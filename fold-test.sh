d4c28e47f4a34b368c6857ccf34d1ff01496a38c
Alhadis <gardnerjohng@gmail.com>
2019-07-09 14:03:58 +1000

    Test what happens with an unclosed fold region

--- fold-test.sh
+++ fold-test.sh
@@ -5,12 +5,33 @@ putBytes(){
 	printf %b `printf \\\\%03o "$@"`
 }
 
-CSI=`putBytes 27 91`
+# Output a control-sequence introducer for an ANSI escape code
+csi(){
+	putBytes 27 91
+}
+
+startFold(){
+	if [ ! "$TRAVIS_JOB_ID" ]; then return; fi
+	set -- "$1" "`csi`"
+	printf 'travis_fold:start:%s\r%s0K' "$1" "$2"
+}
+
+endFold(){
+	if [ ! "$TRAVIS_JOB_ID" ]; then return; fi
+	set -- "$1" "`csi`"
+	printf 'travis_fold:end:%s\r%s0K' "$1" "$2"
+}
 
 echo Before
-printf 'travis_fold:start:foobar_etc\r%s0K%s33;1m%s%s0m\n' "$CSI" "$CSI" "Heading goes here" "$CSI"
+startFold foobar_etc1
+printf '%s33;1m%s%s0m\n' "`csi`" "1. Heading goes here" "`csi`"
 echo Foo
 echo Bar
 echo Baz
-printf 'travis_fold:end:foobar_etc\r%s0K' "$CSI"
+endFold foobar_etc1
 echo After
+startFold foobar_etc2
+printf '%s33;1m%s%s0m\n' "`csi`" "2. Heading goes here" "`csi`"
+echo Foo
+echo Bar
+echo Baz
